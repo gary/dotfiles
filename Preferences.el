4b35bc6ef1d3d91d171a32f9b60694e49cece1cf
sm41Eg01 <sm41Eg01@girlfriend.local>
2006-12-20 20:45:54 +0000

    aquamacs configuration

---
+++ Preferences.el
@@ -0,0 +1,9 @@
+(one-buffer-one-frame-mode 0)
+
+;; transparent frame
+(modify-frame-parameters (selected-frame) '((active-alpha . 0.8)))
+(modify-frame-parameters (selected-frame) '((inactive-alpha . 0.4)))
+
+(cua-mode 0)
+(transient-mark-mode 1)
+
