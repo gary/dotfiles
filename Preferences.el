bc38c8436d0774884e53806811310810b1310eb3
sm41Eg01 <sm41Eg01@girlfriend.local>
2007-01-03 23:52:32 +0000

    minor aquamacs-specific tweaks.  dumped .emacs contents and left a TODO so i'll remember to fix it soon.

--- Preferences.el
+++ Preferences.el
@@ -7,3 +7,26 @@
 (cua-mode 0)
 (transient-mark-mode 1)
 
+(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+(transient-mark-mode 1)
+(iswitchb-mode 1)
+
+;; TODO: reference ~/.emacs for shared functionality
+;; invoke M-x with the Alt key
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+;; prefer backward-kill-word over backspace
+(global-set-key "\C-w" 'backward-kill-word) ;; default M-DEL
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+
+;; lose the UI
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+(global-set-key "\C-x\C-z" 'zap-to-char)
+
+(shell)
