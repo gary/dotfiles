ea09c3d32fb730961080d4c3957e7846bef4cb39
sm41Eg01 <sm41Eg01@girlfriend.local>
2007-01-05 04:07:03 +0000

    aquamacs cfg now references emacs customizations.

--- Preferences.el
+++ Preferences.el
@@ -12,21 +12,5 @@
 (transient-mark-mode 1)
 (iswitchb-mode 1)
 
-;; TODO: reference ~/.emacs for shared functionality
-;; invoke M-x with the Alt key
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
-
-;; prefer backward-kill-word over backspace
-(global-set-key "\C-w" 'backward-kill-word) ;; default M-DEL
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-c\C-k" 'kill-region)
-
-;; lose the UI - percent issues with Aquamacs, for now
-;; (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-
-(global-set-key "\C-x\C-z" 'zap-to-char)
-
-(shell)
+;; load shared customizations
+(load-library "~/.emacs")
