b48c44a14d6db9281828643e70c808db2c3b18b0
(no author) <(no author)@8bbbaef2-aa41-0410-aec7-65436ee0322c>
2007-03-04 23:16:39 +0000

    separating key customizations into a dedicated elisp file.

---
+++ emacs/ekeys.el
@@ -0,0 +1,8 @@
+;; invoke M-x with the Alt key
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+;; prefer backward-kill-word over backspace
+(global-set-key "\C-w" 'backward-kill-word) ;; default M-DEL
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
