2f1620fcce6401d27e5fdb7d860f4e295343c3d4
(no author) <(no author)@8bbbaef2-aa41-0410-aec7-65436ee0322c>
2007-03-07 22:20:58 +0000

    all aquamacs-specific changes moved to customizations.el, which is to become a centralized point of customization for all emacsen.

--- Preferences.el
+++ Preferences.el
@@ -1,10 +1,1 @@
-(one-buffer-one-frame-mode 0)
-
-;; transparent frame
-(modify-frame-parameters (selected-frame) '((active-alpha . 0.8)))
-(modify-frame-parameters (selected-frame) '((inactive-alpha . 0.4)))
-
-(cua-mode 0)
-
-;; load shared customizations
 (load-library "~/.emacs")
