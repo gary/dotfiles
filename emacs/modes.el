77f859e10fb64ae2af6311483b6f5d82cf16bd34
(no author) <(no author)@8bbbaef2-aa41-0410-aec7-65436ee0322c>
2007-03-21 14:13:09 +0000

    ruby mode enhancements: various modes, new defun for evaluating a buffer's contents on the spot.

--- emacs/modes.el
+++ emacs/modes.el
@@ -216,9 +216,19 @@
   "Run an inferior Ruby process" t)
 (autoload 'inf-ruby-keys "inf-ruby"
   "Set local key defs for inf-ruby in ruby-mode")
+(defun ruby-eval-buffer ()
+  (interactive)
+  "Evaluate the buffer with ruby."
+  (shell-command-on-region (point-min) (point-max) "ruby"))
+
+(require 'ruby-electric)
 (add-hook 'ruby-mode-hook
 	  '(lambda ()
-	     (inf-ruby-keys)))
+	     (inf-ruby-keys)
+	     (define-key ruby-mode-map "\C-c\C-a" 'ruby-eval-buffer))
+	     (font-lock-mode t)
+	     (pabbrev-mode t))
+	     (ruby-electric-mode t)))
 (setq ri-ruby-script (concat emacs-root "/modes/ri-emacs.rb"))
 (autoload 'ri "ri-ruby" "Ruby api reference" t)
 
