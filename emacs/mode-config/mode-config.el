6399af626f7b1f3be43a4b899c7a5650541f5381
(no author) <(no author)@8bbbaef2-aa41-0410-aec7-65436ee0322c>
2007-03-25 00:51:41 +0000

    ruby config separated.

--- emacs/mode-config/mode-config.el
+++ emacs/mode-config/mode-config.el
@@ -33,33 +33,6 @@
 ;; 				       interpreter-mode-alist)))
 ;; end java crap
 
-;; ruby and friends
-(autoload 'ruby-mode "ruby-mode"
-  "Mode for editing ruby source files" t)
-(setq auto-mode-alist
-      (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
-(setq interpreter-mode-alist (append '(("ruby" . ruby-mode)
-				       interpreter-mode-alist)))
-(autoload 'run-ruby "inf-ruby"
-  "Run an inferior Ruby process" t)
-(autoload 'inf-ruby-keys "inf-ruby"
-  "Set local key defs for inf-ruby in ruby-mode")
-(defun ruby-eval-buffer ()
-  (interactive)
-  "Evaluate the buffer with ruby."
-  (shell-command-on-region (point-min) (point-max) "ruby"))
-
-(require 'ruby-electric)
-(add-hook 'ruby-mode-hook
-	  '(lambda ()
-	     (inf-ruby-keys)
-	     (define-key ruby-mode-map "\C-c\C-a" 'ruby-eval-buffer)
-	     (font-lock-mode t)
-	     ;; (pabbrev-mode t) doh
-	     (ruby-electric-mode t)))
-(setq ri-ruby-script (concat emacs-root "/modes/ri-emacs.rb"))
-(autoload 'ri "ri-ruby" "Ruby api reference" t)
-
 (autoload 'svn-status "psvn"
   "Examine the status of a Subversion working copy in a directory." t)
 
