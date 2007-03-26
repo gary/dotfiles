b6072f8263758e08e567df7844064a9eebfc3844
(no author) <(no author)@8bbbaef2-aa41-0410-aec7-65436ee0322c>
2007-03-26 17:39:10 +0000

    started the configuration... as it is turning out to be a bitch, the rest is now a TODO.  jde-mode in particular is being anything but cooperative.

--- emacs/mode-config/mmm-mode-config.el
+++ emacs/mode-config/mmm-mode-config.el
@@ -1,2 +1,60 @@
 (setq mmm-global-mode 'maybe)
-(mmm-add-mode-ext-class 'html-mode "\\.jsp\\'" 'html-js)
+(setq mmm-submode-decoration-level 2)
+
+(mmm-add-group 'jsp
+	       `((jsp-code
+		  :submode java
+		  :match-face (("<%!" . mmm-declaration-submode-face)
+			       ("<%=" . mmm-output-submode-face)
+			       ("<%"  . mmm-code-submode-face))
+		  :front "<%[!=]?"
+		  :back "%>"
+		  :insert ((?% jsp-code nil @ "<%" @ " " _ " " @ "%>" @)
+			   (?! jsp-declaration nil @ "<%!" @ " " _ " " @ "%>" @)
+			   (?= jsp-expression nil @ "<%=" @ " " _ " " @ "%>" @))
+		  )
+		 (jsp-directive
+		  :submode text-mode
+		  :face mmm-special-submode-face
+		  :front "<%@"
+		  :back "%>"
+		  :insert ((?@ jsp-directive nil @ "<%@" @ " " _ " " @ "%>" @)))))
+
+(mmm-add-classes
+ '((embedded-ruby
+    :submode ruby-mode
+    :front "<%[=#]?"
+    :back "%>"
+    :insert ((?r eruby-directive nil @ "<%" @ " " _ " " @ "%>" @)
+             (?= eruby-directive nil @ "<%=" @ " " _ " " @ "%>" @)))))
+(mmm-add-classes
+ '((embedded-css
+    :submode css-mode
+    :face mmm-declaration-submode-face
+    :front "style=\""
+    :back "\"")))
+(mmm-add-classes
+ '((embedded-javascript
+    :submode javascript-mode ;; javascript-generic-mode
+    :face mmm-declaration-submode-face
+    :front "<script\[^>\]*>"
+    :back "</script>")))
+(mmm-add-classes
+ '((embedded-javascript-attribute
+    :submode javascript-mode ;; javascript-generic-mode
+    :face mmm-declaration-submode-face
+    :front "\\bon\\w+=\\s-*\""
+    :back "\"")))
+ 
+;; What features should be turned on in this html-mode?
+;; TODO: big mess, better understand how submodes are applied and cleanup accordingly.
+;; (add-to-list 'mmm-mode-ext-classes-alist
+;; 	     '(html-mode nil jsp))
+(add-to-list 'mmm-mode-ext-classes-alist
+	     '(html-mode nil embedded-css))
+(add-to-list 'mmm-mode-ext-classes-alist
+	     '(html-mode nil embedded-ruby))
+(add-to-list 'mmm-mode-ext-classes-alist
+	     '(html-mode nil embedded-javascript))
+(add-to-list 'mmm-mode-ext-classes-alist
+	     '(html-mode nil embedded-javascript-attribute))
