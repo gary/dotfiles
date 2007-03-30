c182de66cc4499077a0673186feebeb611c80b2b
(no author) <(no author)@8bbbaef2-aa41-0410-aec7-65436ee0322c>
2007-03-30 18:29:26 +0000

    new functionality, new customizations... windmove, new speedbar supported extensions, fringe mode setup as a visual pref.

--- emacs/customizations.el
+++ emacs/customizations.el
@@ -2,18 +2,18 @@
 ;; look and feel
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq initial-frame-alist '(
-			    (mouse-color      . "gold")
-			    (foreground-color . "cornsilk2")
-			    (background-color . "grey7")
-			    (top . 25) (left . 10) (width . 125) (height . 50))
+                            (mouse-color      . "gold")
+                            (foreground-color . "cornsilk2")
+                            (background-color . "grey7")
+                            (top . 25) (left . 10) (width . 125) (height . 50))
       )
 
 (setq default-frame-alist '(
-		      (background-color     . "grey7")
-		      (foreground-color     . "cornsilk2")
-		      (cursor-color         . "gold2")
-		      (cursor-type          . box)
-		      (top . 25) (left . 10) (width . 125) (height . 50))
+                      (background-color     . "grey7")
+                      (foreground-color     . "cornsilk2")
+                      (cursor-color         . "gold2")
+                      (cursor-type          . box)
+                      (top . 25) (left . 10) (width . 125) (height . 50))
       )
 
 (if (boundp 'carbon-emacs-package-version)
@@ -21,17 +21,29 @@
       (add-to-list 'initial-frame-alist '(alpha . 65))
       (add-to-list 'default-frame-alist '(alpha . 65))))
 
-
 ;; lose the UI
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+(set-fringe-mode (quote (nil . 0)))
+(setq-default indicate-buffer-boundaries 'left)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; functionality customizations
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (fset 'yes-or-no-p 'y-or-n-p)
 (put 'dired-find-alternate-file 'disabled nil)
+
+(setq recentf-max-saved-items 500)
+(setq recentf-max-mente-items 100)
+
+;; rebind C-x C-f and others to the ffap bindings (see variable ffap-bindings)
+(ffap-bindings)
+;; C-u C-x C-f finds the file at point
+(setq ffap-require-prefix t)
+;; browse urls at point via w3m
+;; TODO: (setq ffap-url-fetcher 'w3m-browse-url)
 
 ;; \M-y calls browse-kill-ring
 ;; (browse-kill-ring-default-keybindings)
@@ -52,10 +64,16 @@
 (setq bm-restore-reposistory-on-load t)
 (setq-default bm-buffer-persistence t)
 
+(setq windmove-wrap-around t)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; always-on modes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq-default indent-tabs-mode nil)             ;; bad tabs, bad
 (setq default-major-mode 'text-mode)
+(if (eq emacs-major-version 22)
+    (ido-mode 1))
+(recentf-mode 1)
 (column-number-mode 1)
 ;;(if (eq invocation-name 'Emacs)
     ;; XEmacs has blink-paren builtin
@@ -74,15 +92,16 @@
 (add-hook 'find-file-hooks 'bm-buffer-restore)
 (add-hook 'kill-buffer-hook 'bm-buffer-save)
 (add-hook 'kill-emacs-hook '(lambda nil
-			      (bm-buffer-save-all)
-			      (bm-repository-save)))
+                              (bm-buffer-save-all)
+                              (bm-repository-save)))
 
 (add-hook 'speedbar-load-hook '(lambda ()
-				 (speedbar-add-supported-extension ".jsp")
-				 (speedbar-add-supported-extension ".r\\(b\\|html\\)")))
+                                 (speedbar-add-supported-extension "\\(\\.xml\\|\\.tld\\)") ;; xml files
+                                 (speedbar-add-supported-extension ".jsp")
+                                 (speedbar-add-supported-extension ".r\\(b\\|html\\)")))
 
 (setq interpreter-mode-alist (append '(("ruby" . ruby-mode)
-				       interpreter-mode-alist)))
+                                       interpreter-mode-alist)))
 
 (if (not (boundp 'carbon-emacs-package-version))
     (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode)))
