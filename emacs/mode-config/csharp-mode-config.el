69d5c793425a3b732e35894af50eb7da96b7c713
(no author) <(no author)@8bbbaef2-aa41-0410-aec7-65436ee0322c>
2007-04-28 22:30:51 +0000

    unheard of, if not for reviewing some csharp code for fingers.

---
+++ emacs/mode-config/csharp-mode-config.el
@@ -0,0 +1,23 @@
+;; Import C# mode
+(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
+(setq auto-mode-alist
+(append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
+
+;; C/C++/Java/C# Mode
+(defun my-c-mode-common-hook ()
+  (c-set-offset 'substatement-open 0)
+  (c-set-offset 'statement-cont 4)
+  (c-set-offset 'topmost-intro-cont 0)
+  (c-set-offset 'block-open 0)
+  (c-set-offset 'arglist-intro 4)
+  (c-set-offset 'arglist-cont-nonempty 4))
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+(defun my-csharp-mode-hook ()
+  (progn
+   (turn-on-font-lock)
+   (auto-fill-mode)
+   (setq tab-width 4)
+   (define-key csharp-mode-map "\t" 'c-tab-indent-or-complete)))
+(add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
