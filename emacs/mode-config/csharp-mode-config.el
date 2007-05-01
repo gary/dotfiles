4afb8c9577896243f827fd41129203ca246093af
(no author) <(no author)@8bbbaef2-aa41-0410-aec7-65436ee0322c>
2007-05-01 02:44:27 +0000

    moving extension recognition code to customizations.el... where it should have been from the start.

--- emacs/mode-config/csharp-mode-config.el
+++ emacs/mode-config/csharp-mode-config.el
@@ -1,7 +1,5 @@
 ;; Import C# mode
 (autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
-(setq auto-mode-alist
-(append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
 
 ;; C/C++/Java/C# Mode
 (defun my-c-mode-common-hook ()
