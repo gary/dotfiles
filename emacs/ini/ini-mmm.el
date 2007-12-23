a2e01ac93221498724221a9b64f3bfefeec033b8
ge.iams <ge.iams@gmail.com>
2007-12-23 16:44:11 +0000

    - reformatted to conform to ini file conventions.

--- emacs/ini/ini-mmm.el
+++ emacs/ini/ini-mmm.el
@@ -1,12 +1,25 @@
+;;; ==================================================================
+;;; Author:  gary iams
+;;; File:    ini-mmm
+;;; Purpose: Setups for Multiple Major Modes
+;;; ==================================================================
+
+(require 'mmm-auto)
+
+;;; Setups for MMM mode ==============================================
+
 (setq mmm-global-mode 'maybe)
 (setq mmm-submode-decoration-level 2)
 
-;; TODO:
+;;; New mode combinations --------------------------------------------
+
+(mmm-add-mode-ext-class nil "\\.jsp$" 'embedded-css)
+(mmm-add-mode-ext-class nil "\\.jsp$" 'html-js)
+(mmm-add-mode-ext-class nil "\\.jsp$" 'jsp) ;;
+
+;;; Color adjustments ------------------------------------------------
+
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
  '(mmm-cleanup-submode-face ((t (:background "Wheat" :foreground "black"))))
  '(mmm-code-submode-face ((t (:background "LightGray" :foreground "black"))))
  '(mmm-comment-submode-face ((t (:background "SkyBlue" :foreground "black"))))
@@ -15,18 +28,3 @@
  '(mmm-init-submode-face ((t (:background "Pink" :foreground "black"))))
  '(mmm-output-submode-face ((t (:background "Plum" :foreground "black"))))
  '(mmm-special-submode-face ((t (:background "MediumSpringGreen" :foreground "black")))))
-
-(mmm-add-mode-ext-class nil "\\.jsp$" 'embedded-css)
-(mmm-add-mode-ext-class nil "\\.jsp$" 'html-js)
-(mmm-add-mode-ext-class nil "\\.jsp$" 'jsp) ;;
-
-;; (mmm-add-classes
-;;  '((embedded-ruby
-;;     :submode ruby-mode
-;;     :front "<%[=#]?"
-;;     :back "%>"
-;;     :insert ((?r eruby-directive nil @ "<%" @ " " _ " " @ "%>" @)
-;;              (?= eruby-directive nil @ "<%=" @ " " _ " " @ "%>" @)))))
-
-;; (mmm-add-mode-ext-class nil "\\.rb$" 'embedded-ruby)
-;; (mmm-add-mode-ext-class nil "\\.rhtml$" 'embedded-ruby)
