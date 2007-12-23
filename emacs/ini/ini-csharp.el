133feffb847e0908774082d2e92c88d9a2f6b0d6
ge.iams <ge.iams@gmail.com>
2007-12-23 00:44:55 +0000

    - contents updated to conform to ini file standards for documentation.

--- emacs/ini/ini-csharp.el
+++ emacs/ini/ini-csharp.el
@@ -1,8 +1,14 @@
-;; Import C# mode
+;;; ==================================================================
+;;; Author:  gary iams
+;;; File:    ini-csharp
+;;; Purpose: Setups for CSharp mode
+;;; ==================================================================
+
+;;; auto loads -------------------------------------------------------
+
 (autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
 
-;; C/C++/Java/C# Mode
-(defun my-c-mode-common-hook ()
+(defun gi-csharp-defaults ()
   (c-set-offset 'substatement-open 0)
   (c-set-offset 'statement-cont 4)
   (c-set-offset 'topmost-intro-cont 0)
@@ -10,12 +16,8 @@
   (c-set-offset 'arglist-intro 4)
   (c-set-offset 'arglist-cont-nonempty 4))
 
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+(defun gi-csharp-init-keys ()
+  (define-key csharp-mode-map [tab] 'c-tab-indent-or-complete))
 
-(defun my-csharp-mode-hook ()
-  (progn
-   (turn-on-font-lock)
-   (auto-fill-mode)
-   (setq tab-width 4)
-   (define-key csharp-mode-map "\t" 'c-tab-indent-or-complete)))
-(add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
+(add-hook 'csharp-mode-hook 'gi-sharp-defaults)
+(add-hook 'csharp-mode-hook 'gi-init-keys)
