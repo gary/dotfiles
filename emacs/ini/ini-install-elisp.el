7040ddf687599809564867819a7c6924b7ae8772
ge.iams <ge.iams@gmail.com>
2008-01-02 02:25:46 +0000

    - hey look, i found a way to streamline the installation of new elisp.

---
+++ emacs/ini/ini-install-elisp.el
@@ -0,0 +1,10 @@
+;;; ==================================================================
+;;; Author: gary iams
+;;; File:   ini-install-elisp.el
+;;; Purpose: Setups for the Simple Emacs Lisp Installer
+;;; ==================================================================
+
+;;; Setups for Elisp Installer =======================================
+
+(require 'install-elisp)
+(setq install-elisp-repository-directory root-directory)
