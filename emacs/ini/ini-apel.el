92f5cfd5eaef81bfb49c246076f4c4dc930d0b57
ge.iams <ge.iams@gmail.com>
2008-04-06 18:33:42 +0000

    - adding the APEL package as a dependency, removing the last of my carbon emacs chains.

---
+++ emacs/ini/ini-apel.el
@@ -0,0 +1,7 @@
+;;; ==================================================================
+;;; Author:  gary iams
+;;; File:    ini-apel
+;;; Purpose: Setups for APEL, "A Portable Emacs Library"
+;;; ==================================================================
+
+(attach-package "/apel")
