17e8127f4cacee1f71db5ea2f0dee6a91c8e7168
ge.iams <ge.iams@gmail.com>
2008-04-06 16:23:58 +0000

    - direct use of dabbrev expansion abandoned in favor of hippie-expand.

--- emacs/ini/ini-dabbrev-highlight.el
+++
@@ -1,7 +0,0 @@
-;;; ==================================================================
-;;; Author:  gary iams
-;;; File:    ini-dabbrev-highlight
-;;; Purpose: Setups for dabbrev highlighting
-;;; ==================================================================
-
-(require 'dabbrev-highlight)
