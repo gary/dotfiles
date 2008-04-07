6d884a6c79a70fe64e8c433a397be461e64bfa6e
ge.iams <ge.iams@gmail.com>
2008-04-07 03:42:36 +0000

    - resolved browser issue by setting the browser function os x's default when on os x, or firefox if not.  smooth sailing now.

--- emacs/ini/ini-browse.el
+++ emacs/ini/ini-browse.el
@@ -4,14 +4,13 @@
 ;;; Purpose: Setups for browse-url
 ;;; ==================================================================
 
-(setq browse-url-browser-function 'browse-url-netscape)
-;;;      (if (at-home) 'browse-url-netscape 'browse-url-mosaic))
-
+(if (on-osx)
+    (setq browse-url-browser-function 'browse-url-default-macosx-browser)
+  (setq browse-url-browser-function 'browse-url-firefox))
 
 (autoload browse-url-browser-function "new-browse-url.el"
   "Ask a WWW browser to show a URL." t)
 
-(global-set-key "\C-xw" browse-url-browser-function)
 (if (not (is-xemacs))
     (global-set-key [S-mouse-1] 'browse-url-at-mouse))
 
