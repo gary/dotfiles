235bc36814b7aff8d55f9b1296db5f41b2f421df
ge.iams <ge.iams@gmail.com>
2008-04-20 04:35:24 +0000

    - updated to 1.3.

--- emacs/packages/rinari/toggle.el
+++ emacs/packages/rinari/toggle.el
@@ -3,14 +3,20 @@
 ;; Copyright (C) 2006-2007 by Ryan Davis
 
 ;; Author: Ryan Davis <ryand-ruby@zenspider.com>
-;; Version 1.1
+;; Version 1.3
 ;; Keywords: files, extensions, convenience
 ;; Created: 2006-03-22
 ;; Compatibility: Emacs 22, 21?
 ;; URL(en): http://seattlerb.rubyforge.org/
 
-;; The MIT License: http://en.wikipedia.org/wiki/MIT_License
+;;; Posted using:
+;; (setq emacs-wiki-name "RyanDavis")
+;; (wikiput-buffer "update")
 
+;;; The MIT License:
+
+;; http://en.wikipedia.org/wiki/MIT_License
+;;
 ;; Permission is hereby granted, free of charge, to any person obtaining
 ;; a copy of this software and associated documentation files (the
 ;; "Software"), to deal in the Software without restriction, including
@@ -35,60 +41,77 @@
 ;; This package provides the ability to quickly open a corresponding
 ;; file for the current buffer by using a bi-directional mapping of
 ;; regular expression pairs. You can select a mapping style from
-;; `toggle-mapping-styles' by setting the `toggle-mapping-style'
-;; variable.
+;; `toggle-mapping-styles' using the `toggle-style' function or set
+;; your default style via the `toggle-mapping-style' variable.
 
-;; In addition to bi-directional regular expression mapping, you may
-;; also put a function (named or lambda) in the second cell of the
-;; mapping cons, and that function will be called when the rule is
-;; invoked. See the rails-view rule for an example.
-
-;; There are 4 different mapping styles in this version: zentest,
-;; rails-test, rails-view, and ruby. Feel free to submit more and I'll
-;; incorporate them.
+;; There are 3 different mapping styles in this version: zentest,
+;; rails, and ruby. Feel free to submit more and I'll incorporate
+;; them.
 
 ;;; Example Mapping (ruby style):
 ;;
 ;; blah.rb <-> test_blah.rb
 ;; lib/blah.rb <-> test/test_blah.rb
 
+;;; History:
+
+;; 1.3 2007-05-10 Added tab completion to toggle-style. Suggested by TingWang.
+;; 1.2 2007-04-06 Interleave bidirectional mappings. Fixed interactive setter. Added rspec mappings.
+;; 1.1 2007-03-30 Initial release to emacswiki.org. Added named styles and bidi.
+;; 1.0 2006-03-22 Birfday.
+
+(require 'cl)
+
 (defcustom toggle-mapping-styles
-  '((zentest    . (("app/controllers/\\1.rb" . "test/controllers/\\1_test.rb")
-		   ("app/views/\\1.rb"       . "test/views/\\1_test.rb")
-		   ("app/models/\\1.rb"      . "test/unit/\\1_test.rb")
-		   ("lib/\\1.rb"             . "test/unit/test_\\1.rb")))
-    (rails-test . (("app/controllers/\\1.rb" . "test/functional/\\1_test.rb")
-		   ("app/models/\\1.rb"      . "test/unit/\\1_test.rb")
-		   ("lib/\\1.rb"             . "test/unit/test_\\1.rb")))
-    (rails-view . (("app/controllers/\\(.*\\).rb" . toggle-rails-action-to-view) ; could also use a lambda here
-		   ("app/views/\\(.*\\)/\\(.*\\).rhtml" . toggle-rails-view-to-action)))
-    (ruby-test  . (("lib/\\1.rb"             . "test/test_\\1.rb")
-		   ("\\1.rb"                 . "test_\\1.rb"))))
+  '((zentest . (("app/controllers/\\1.rb" . "test/controllers/\\1_test.rb")
+                ("app/views/\\1.rb"       . "test/views/\\1_test.rb")
+                ("app/models/\\1.rb"      . "test/unit/\\1_test.rb")
+                ("lib/\\1.rb"             . "test/unit/test_\\1.rb")))
+    (rspec   . (("app/models/\\1.rb"      . "spec/models/\\1_spec.rb")
+                ("app/controllers/\\1.rb" . "spec/controllers/\\1_spec.rb")
+                ("app/views/\\1.rb"       . "spec/views/\\1_spec.rb")
+                ("app/helpers/\\1.rb"     . "spec/helpers/\\1_spec.rb")))
+    (rails   . (("app/controllers/\\1.rb" . "test/functional/\\1_test.rb")
+                ("app/models/\\1.rb"      . "test/unit/\\1_test.rb")
+                ("lib/\\1.rb"             . "test/unit/test_\\1.rb")))
+    (ruby    . (("lib/\\1.rb"             . "test/test_\\1.rb")
+                ("\\1.rb"                 . "test_\\1.rb"))))
   "A list of (name . toggle-mapping) rules used by toggle-filename."
   :group 'toggle
   :type '(repeat (cons string string)))
 
-(defun toggle-style (name)
-  (interactive "sStyle: ")
-  (let ((pairs (cdr (assoc name toggle-mapping-styles))))
-    (if pairs
-	(mapcar (lambda (pair)
-		  (if (stringp (cdr pair)) ; pairs that contain functions aren't bidirectional
-		      (cons (replace-regexp-in-string "\\\\1" "\\\\(.*\\\\)"
-						      (car pair)) (cdr pair))
-		    pair))
-		;; each pair implictly includes its reverse
-		(remove nil (append pairs
-				    (mapcar (lambda (pair)
-					      (if (stringp (cdr pair))
-						  (cons (cdr pair) (car pair))))
-					      pairs)))))))
-
 (defcustom toggle-mapping-style
-  'rails-test
-  "The defaulte toggle mapping style to load when initialized."
+  'rspec
+  "The default toggle mapping style to load when initialized."
   :group 'toggle
   :type '(symbol))
+
+(defun toggle-style (name)
+  (interactive (list (completing-read "Style: "
+                                      (mapcar
+                                       #'symbol-name
+                                       (mapcar #'car toggle-mapping-styles))
+                                      nil t "")))
+  (let* ((style (if (stringp name) (intern name) name))
+         (pairs (cdr (assoc style toggle-mapping-styles))))
+    (if pairs
+        (let ((mappings
+               (mapcar (lambda (pair)
+                         (cons
+                          (replace-regexp-in-string
+                           "\\\\1" "\\\\(.*\\\\)"
+                           (replace-regexp-in-string ; special case for "\\1.ext"
+                            "^\\\\1" "\\\\([^/]*\\\\)" (car pair)))
+                          (cdr pair)))
+                       (mapcan 'list
+                               pairs
+                               (mapcar (lambda (pair)
+                                         (cons (cdr pair) (car pair)))
+                                       pairs)))))
+          (if (interactive-p)
+              (setq toggle-mappings mappings)
+            mappings))
+      nil)))
 
 (defvar toggle-mappings (toggle-style toggle-mapping-style)
   "*The current file mappings for `toggle-filename' to use.")
@@ -101,30 +124,17 @@ TRANS. After the first successful match, this returns. If no rule
 matches, it returns nil"
   (cond ((null rules) nil)
     ((string-match (caar rules) path)
-     (if (stringp (cdar rules))
-	 (replace-match (cdar rules) nil nil path)
-       (cdar rules)))
+     (replace-match (cdar rules) nil nil path))
     (t (toggle-filename path (rest rules)))))
 
-(defun toggle-buffer (&optional mappings)
+(defun toggle-buffer ()
   "Opens a related file to the current buffer using matching rules.
 Matches the current buffer against rules in toggle-mappings. If a
 match is found, switches to that buffer."
   (interactive)
-  (if (symbolp mappings) ; for toggling according to non-default mappings
-      (setq mappings (toggle-style mappings)))
-  (let ((mapping (toggle-filename (buffer-file-name) (or mappings toggle-mappings))))
-    (if (stringp mapping)
-	(find-file mapping)
-      (if (functionp mapping)
-	  (funcall mapping)
-	(message (concat "Match not found for " (buffer-file-name)))))))
-
-;; TODO: move implementation from Rinari to here.
-(defun toggle-rails-action-to-view ()
-  (message "action to view"))
-(defun toggle-rails-view-to-action ()
-  (message "view to action"))
+  (let ((new-name (toggle-filename (buffer-file-name) toggle-mappings)))
+    (if new-name
+        (find-file new-name)
+      (message (concat "Match not found for " (buffer-file-name))))))
 
 (provide 'toggle)
-
