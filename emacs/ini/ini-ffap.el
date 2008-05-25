c73eff4c77de71518567ea9ed4b04fa632d76cf0
ge.iams <ge.iams@gmail.com>
2008-05-25 04:33:05 +0000

    - FFAP for ruby require statements

--- emacs/ini/ini-ffap.el
+++ emacs/ini/ini-ffap.el
@@ -19,3 +19,17 @@
 ;;                    (string-to-number (match-string 1)))))
 ;;     ad-do-it
 ;;     (and line (goto-line line))))
+
+;;; Find Ruby Files in requires --------------------------------------
+
+(defun ruby-module-path(module)
+  (shell-command-to-string 
+   (concat 
+    "ruby -e " 
+    "\"ret='()';$LOAD_PATH.each{|p| " 
+    "x=p+'/'+ARGV[0].gsub('.rb', '')+'.rb';" 
+    "ret=File.expand_path(x)" 
+    "if(File.exist?(x))};printf ret\" " 
+    module)))
+
+(push '(ruby-mode . ruby-module-path) ffap-alist)
