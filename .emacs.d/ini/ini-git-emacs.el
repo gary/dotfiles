262d72eff7b7dfb0963970b3fb1e82c4fafcf2d0
gary <ge.iams@gmail.com>
2008-08-24 01:20:44 -0400

    making the switch to magit official.

--- .emacs.d/ini/ini-git-emacs.el
+++
@@ -1,8 +0,0 @@
-;;; ==================================================================
-;;; Author:  gary iams
-;;; File:    ini-git-emacs
-;;; Purpose: Setups for the emacs-git
-;;; ==================================================================
-
-(attach-package "/git-emacs")
-(add-to-load-path (concat package-directory "/git-emacs"))
