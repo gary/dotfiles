3710b1c4d00aa232320c1371b2a2b6b2a127e7cf
gary <ge.iams@gmail.com>
2008-09-07 17:41:49 -0400

    add YASnippet expansion to Hippie Expand's function list.

--- .emacs.d/ini/ini-yasnippet.el
+++ .emacs.d/ini/ini-yasnippet.el
@@ -6,7 +6,11 @@
 
 (require 'yasnippet)
 
-;; Setups for yasnippet ==============================================
+;;; Setups for yasnippet =============================================
 
 (yas/initialize)
 (yas/load-directory snippet-directory)
+
+;;; Integration with Hippie Expand -----------------------------------
+
+(add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)
