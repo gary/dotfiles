eb4d36fa785d1c7954b507f33bbcfcf8a8915a52
gary <ge.iams@gmail.com>
2008-11-07 11:10:43 -0500

    setups for markdown mode

---
+++ .emacs.d/ini/ini-markdown.el
@@ -0,0 +1,7 @@
+;;; ==================================================================
+;;; Author:  gary iams
+;;; File:    ini-pastie
+;;; Purpose: Setups for Markdown Mode
+;;; ==================================================================
+
+(attach-package "/markdown-mode")
