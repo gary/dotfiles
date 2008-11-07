aa16cc2557dba5756f41912a31c84ee7de1a9746
gary <ge.iams@gmail.com>
2008-11-07 10:58:33 -0500

    gist config updated

--- .emacs.d/ini/ini-gist.el
+++ .emacs.d/ini/ini-gist.el
@@ -1,10 +1,13 @@
 ;;; ==================================================================
 ;;; Author:  gary iams
 ;;; File:    ini-pastie
-;;; Purpose: Setups for Pastie
+;;; Purpose: Setups for Gist
 ;;; ==================================================================
+
+(attach-package "/gist.el")
 
 ;;; Key Bindings -----------------------------------------------------
 
 (global-set-key "\C-c\C-kb" 'gist-buffer)
 (global-set-key "\C-c\C-kr" 'gist-region)
+(global-set-key "\C-c\C-kg" 'gist-fetch)
